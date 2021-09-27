a0001a975bdddb31739138de2f8db6a73c06c274
ClementTsai <90400408+ClementTsai@users.noreply.github.com>
2021-09-27 16:58:04 -0400

    Add files via upload

---
+++ HappinessVMedalsProj.sql
@@ -0,0 +1,56 @@
+-- Skills Used: Joins, Aggregate Functions, CTE's
+-- Top 5 Countries with Highest Ladder Score compared to Total Medals won
+SELECT h.country_name, h.ladder_score, MAX(m.total) as total_medals, MAX((m.total/h.ladder_score)) AS MedalsPerScorePt
+FROM Happiness h
+JOIN Medals m 
+ON h.country_name=m.country_name
+GROUP BY h.country_name
+ORDER BY MedalsPerScorePt desc
+limit 5;
+
+-- How many countries have a score greater than 5
+SELECT COUNT(country_name)
+FROM Happiness
+WHERE ladder_score >= 5;
+
+-- How many countries with at least one gold have a ladder score greater than five
+SELECT COUNT(h.country_name) AS 1Gold5ScoreCount
+FROM Happiness h 
+JOIN medals m 
+ON h.country_name=m.country_name
+WHERE ladder_score >= 5 
+AND gold >= 1;
+
+-- Breaking Things Down by Regions
+-- Show regions with highest medal tally
+SELECT h.rg_id, SUM(m.total) AS medaltally
+FROM Happiness h 
+JOIN Medals m
+ON h.country_name=m.country_name
+GROUP BY h.rg_id
+ORDER BY medaltally DESC;
+
+-- Percentge of countries that won at least one(1) gold
+-- First CTE
+WITH TotalCountryCount (CountCountry)
+AS
+(
+SELECT COUNT(country_name) AS CountCountry
+FROM medals
+),
+-- Second CTE
+CountriesWGold (GoldCount)
+AS
+(
+SELECT COUNT(country_name) AS GoldCount
+FROM Medals
+WHERE gold >= 1
+)
+-- Using both
+SELECT *, (GoldCount/CountCountry)*100 AS CountriesWithGoldPercent
+FROM TotalCountryCount
+CROSS JOIN CountriesWGold;
+
+
+select COUNT(DISTINCT(rg_id))
+from happiness
